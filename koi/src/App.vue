5add53a0ed1ddf40ff647c52280ef31130fe9f2b
Austin Shadel <austinshadel795@gmail.com>
2023-06-16 23:28:10 -0700

    Setup

---
+++ koi/src/App.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+
+import { ref } from 'vue'
+
+const items = ref([{ koi: 'Kohaku', file:'koi_1.svg' }, 
+                   { koi: 'Ki Utsuri', file: 'koi_2.svg' },
+                   { koi: 'Tancho Showa', file: 'koi_3.svg' },
+                   { koi: 'Kohaku', file: 'koi_4.svg' },
+                   { koi: 'Tancho Showa', file: 'koi_5.svg' },
+                   { koi: 'Kohaku', file: 'koi_6.svg' },
+                   { koi: 'Kohaku', file: 'koi_7.svg' },
+                   { koi: 'Kohaku', file: 'koi_8.svg' },
+                   { koi: 'Hi Utsuri', file: 'koi_9.svg' },
+                   { koi: 'Kohaku', file: 'koi_10.svg' },
+                  ])
+
+</script>
+
+<template>
+  <div class="static grid grid-cols-5 gap-4">
+      <button class="border-transparent p-5" v-for="(item) in items">
+        <a class="h-50 w-25">
+          <img :src="`../src/assets/${item.file}`" />  
+        </a>
+        <br>
+        <div class="static bottom-0 left-0">
+          <h1 class="font-bold font-mono font-black hover:italic">{{ `${item.koi}` }}</h1>
+        </div>
+      </button>
+  </div>
+</template>
+
+<style scoped>
+</style>
---
+++ koi/src/components/HelloWorld.vue
@@ -0,0 +1,25 @@
+<script>
+
+</script>
+
+<template>
+  <div class="static grid grid-cols-5 gap-4">
+    <li v-for="(item, index) in items">
+  	    {{ index }} - {{ item.message }}
+	  </li>
+    <!-- <button class="border-transparent p-5" v-for="n in 10" :key="n">
+      <a class="h-50 w-25">
+        <img :src="`../src/assets/koi_${n}.svg`" />  
+      </a>
+      <br>
+      <div class="static bottom-0 left-0">
+        <h1 class="font-bold font-mono font-black hover:italic">{{ `Tancho Showa ${n}` }}</h1>
+      </div>
+    </button>-->
+  </div>
+</template>
+
+<style scoped>
+</style>
+
+
